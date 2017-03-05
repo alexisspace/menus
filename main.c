ec7828dc3c082a00da8525e7cbd5b2e1f44b5a00
alexisspace <alexis.guillermo.ortega.acurero@gmail.com>
2017-03-05 13:11:10 -0400

    Main menu outline added

--- main.c
+++ main.c
@@ -41,9 +41,21 @@
 #include "Images/images.h"
 
 // Colors for graphics
-#define TITLE_COLOR    GRAPHICS_COLOR_FOREST_GREEN
-#define BAR_COLOR       GRAPHICS_COLOR_FOREST_GREEN
+#define TITLE_COLOR    GRAPHICS_COLOR_DARK_ORANGE//GRAPHICS_COLOR_AQUA//GRAPHICS_COLOR_FOREST_GREEN
+#define BAR_COLOR       GRAPHICS_COLOR_DARK_ORANGE//GRAPHICS_COLOR_AQUA //GRAPHICS_COLOR_FOREST_GREEN
 #define DATA_COLOR      GRAPHICS_COLOR_WHITE
+#define NORMAL_COLOR    GRAPHICS_COLOR_FOREST_GREEN
+#define WARN_COLOR      GRAPHICS_COLOR_YELLOW//GRAPHICS_COLOR_ORANGE
+#define DANGER_COLOR    GRAPHICS_COLOR_RED
+
+// Main Menus Colors
+#define MMENU_TITLE_COLOR               GRAPHICS_COLOR_WHITE
+#define MMENU_BACKGROUND_COLOR          GRAPHICS_COLOR_BLACK//0x00C5D1D7
+#define MMENU_TITLE_BACKGROUND_COLOR    GRAPHICS_COLOR_DARK_RED
+#define MMENU_BOTTOM_BAR_COLOR          GRAPHICS_COLOR_DARK_RED//0x008CADAE//GRAPHICS_COLOR_DARK_GRAY//GRAPHICS_COLOR_LIGHT_SEA_GREEN
+#define MMENU_LEFT_BAR_COLOR            GRAPHICS_COLOR_DARK_RED
+#define MMENU_DATA_FONT_COLOR           GRAPHICS_COLOR_WHITE
+
 
 //Touch screen context
 touch_context g_sTouchContext;
@@ -63,7 +75,7 @@ void timerInit(void);
 void initializeDemoButtons(void);
 void drawInitScreen(void);
 void drawMainView(void);
-void drawMainMenu(void);
+void drawSystemSetupMenu(void);
 void runPrimitivesDemo(void);
 void runImagesDemo(void);
 void drawRestarDemo(void);
@@ -94,6 +106,11 @@ void main(void)
 
     //Enter main view
     drawMainView();
+    Delay();
+
+    drawSystemSetupMenu();
+    Delay();
+
 
     //runImagesDemo();
     //runPrimitivesDemo();
@@ -131,53 +148,79 @@ void drawMainView(void)
     Graphics_setFont(&g_sContext, &g_sFontCmss16b);
     Graphics_clearDisplay(&g_sContext);
 
+/*
     // Status information area
     Graphics_drawString(&g_sContext, "Status: ",
                                 AUTO_STRING_LENGTH, 0, 0, GRAPHICS_TRANSPARENT_TEXT);
-
+*/
 
     // First line of information
     Graphics_setFont(&g_sContext, &g_sFontCmss18b);
     Graphics_drawStringCentered(&g_sContext, "Surface Time",
-                                AUTO_STRING_LENGTH, 53, 70, GRAPHICS_TRANSPARENT_TEXT);
+                                AUTO_STRING_LENGTH, 53, 9, GRAPHICS_TRANSPARENT_TEXT);
     Graphics_drawStringCentered(&g_sContext, "Dive Time",
-                                AUTO_STRING_LENGTH, 155, 70, GRAPHICS_TRANSPARENT_TEXT);
+                                AUTO_STRING_LENGTH, 155, 9, GRAPHICS_TRANSPARENT_TEXT);
     Graphics_drawStringCentered(&g_sContext, "Max Depth",
-                                AUTO_STRING_LENGTH, 255, 70, GRAPHICS_TRANSPARENT_TEXT);
+                                AUTO_STRING_LENGTH, 255, 9, GRAPHICS_TRANSPARENT_TEXT);
 
     // First line data
     Graphics_setForegroundColor(&g_sContext, DATA_COLOR);
     Graphics_setFont(&g_sContext, &g_sFontCmss22b);
     Graphics_drawStringCentered(&g_sContext, "999:99",
-                                AUTO_STRING_LENGTH, 53, 90, GRAPHICS_TRANSPARENT_TEXT);
+                                AUTO_STRING_LENGTH, 53, 29, GRAPHICS_TRANSPARENT_TEXT);
 
     Graphics_setFont(&g_sContext, &g_sFontCmss28b);
     Graphics_drawStringCentered(&g_sContext, "99:99",
-                                AUTO_STRING_LENGTH, 155, 95, GRAPHICS_TRANSPARENT_TEXT);
-    Graphics_drawStringCentered(&g_sContext, "9999 ft",
-                                AUTO_STRING_LENGTH, 255, 95, GRAPHICS_TRANSPARENT_TEXT);
+                                AUTO_STRING_LENGTH, 155, 32, GRAPHICS_TRANSPARENT_TEXT);
+    Graphics_drawStringCentered(&g_sContext, "9999",
+                                AUTO_STRING_LENGTH, 255, 32, GRAPHICS_TRANSPARENT_TEXT);
+
+    Graphics_setForegroundColor(&g_sContext, TITLE_COLOR);
+    Graphics_setFont(&g_sContext, &g_sFontCmss18b);
+    Graphics_drawString(&g_sContext, "ft",
+                                AUTO_STRING_LENGTH, 290, 28, GRAPHICS_TRANSPARENT_TEXT);
 
     // ppO2 sensors readings
     Graphics_setForegroundColor(&g_sContext, BAR_COLOR);
-    hBar.yMin += 115; // Add Y offset
-    hBar.yMax += 115;
+    hBar.yMin = 55; //
+    hBar.yMax = 57;
     Graphics_fillRectangle(&g_sContext, &hBar);
 
     Graphics_setForegroundColor(&g_sContext, TITLE_COLOR);
     Graphics_setFont(&g_sContext, &g_sFontCmss18b);
     Graphics_drawStringCentered(&g_sContext, "ppO2 Readings",
-                                AUTO_STRING_LENGTH, 150, 130, GRAPHICS_TRANSPARENT_TEXT);
+                                AUTO_STRING_LENGTH, 150, 70, GRAPHICS_TRANSPARENT_TEXT);
 
-    Graphics_setForegroundColor(&g_sContext, GRAPHICS_COLOR_LIGHT_GREEN);
+    Graphics_setForegroundColor(&g_sContext, NORMAL_COLOR);
     Graphics_setFont(&g_sContext, &g_sFontCmss44b);
     Graphics_drawStringCentered(&g_sContext, "1.00",
-                                AUTO_STRING_LENGTH, 53, 160, GRAPHICS_TRANSPARENT_TEXT);
-    Graphics_drawStringCentered(&g_sContext, "1.00",
-                                AUTO_STRING_LENGTH, 159, 160, GRAPHICS_TRANSPARENT_TEXT);
-    Graphics_drawStringCentered(&g_sContext, "1.00",
-                                AUTO_STRING_LENGTH, 265, 160, GRAPHICS_TRANSPARENT_TEXT);
+                                AUTO_STRING_LENGTH, 48, 100, GRAPHICS_TRANSPARENT_TEXT);
 
+    Graphics_setForegroundColor(&g_sContext, WARN_COLOR);
+    Graphics_drawStringCentered(&g_sContext, "1.00",
+                                AUTO_STRING_LENGTH, 156, 100, GRAPHICS_TRANSPARENT_TEXT);
+    Graphics_setForegroundColor(&g_sContext, DANGER_COLOR);
+    Graphics_drawStringCentered(&g_sContext, "1.00",
+                                AUTO_STRING_LENGTH, 262, 100, GRAPHICS_TRANSPARENT_TEXT);
 
+    Graphics_setForegroundColor(&g_sContext, BAR_COLOR);
+    hBar.yMin = 122; //
+    hBar.yMax = 124;
+    Graphics_fillRectangle(&g_sContext, &hBar);
+
+    // Middle space
+    Graphics_setForegroundColor(&g_sContext, TITLE_COLOR);
+    Graphics_setFont(&g_sContext, &g_sFontCmss18b);
+    Graphics_drawStringCentered(&g_sContext, "Depth",
+                                AUTO_STRING_LENGTH, 35, 135, GRAPHICS_TRANSPARENT_TEXT);
+    Graphics_setForegroundColor(&g_sContext, DATA_COLOR);
+    Graphics_setFont(&g_sContext, &g_sFontCmss28b);
+    Graphics_drawStringCentered(&g_sContext, "9999",
+                                AUTO_STRING_LENGTH, 35, 158, GRAPHICS_TRANSPARENT_TEXT);
+    Graphics_setForegroundColor(&g_sContext, TITLE_COLOR);
+    Graphics_setFont(&g_sContext, &g_sFontCmss18b);
+    Graphics_drawString(&g_sContext, "ft",
+                                AUTO_STRING_LENGTH, 70, 154, GRAPHICS_TRANSPARENT_TEXT);
 
     // Third line of information
     Graphics_setForegroundColor(&g_sContext, DATA_COLOR);
@@ -218,11 +261,108 @@ void drawMainView(void)
 
 
 }
-void drawMainMenu(void)
+void drawSystemSetupMenu(void)
 {
-    Graphics_setForegroundColor(&g_sContext, GRAPHICS_COLOR_BLACK);
-    Graphics_setBackgroundColor(&g_sContext, GRAPHICS_COLOR_WHITE);
+    Graphics_Rectangle rectangle1 = { 0, 0, 319, 239};
+
+
+    //********** Draw outline **************************************
+    Graphics_setForegroundColor(&g_sContext, MMENU_BACKGROUND_COLOR);
+    Graphics_setBackgroundColor(&g_sContext, MMENU_BACKGROUND_COLOR);
+
+    // Draw background
     Graphics_clearDisplay(&g_sContext);
+
+    // Draw screen border
+    //Graphics_drawRectangle (&g_sContext, &rectangle1);
+
+    // Draw title rectangle
+    rectangle1.xMin = 0;
+    rectangle1.xMax = 319;
+    rectangle1.yMin = 0;
+    rectangle1.yMax = 54;
+    Graphics_setForegroundColor(&g_sContext, MMENU_TITLE_BACKGROUND_COLOR);
+    Graphics_fillRectangle (&g_sContext, &rectangle1);
+
+    // Draw title line
+    Graphics_setForegroundColor(&g_sContext, MMENU_BACKGROUND_COLOR);
+    Graphics_drawLineH (&g_sContext, 0, 319, 50);
+/*
+    // Draw left rectangle
+    rectangle1.xMin = 0;
+    rectangle1.xMax = 9;
+    rectangle1.yMin = 55;
+    rectangle1.yMax = 209;
+    Graphics_setForegroundColor(&g_sContext, MMENU_LEFT_BAR_COLOR);
+    Graphics_fillRectangle (&g_sContext, &rectangle1);
+*/
+
+    // Draw bottom rectangle
+    rectangle1.xMin = 0;
+    rectangle1.xMax = 319;
+    rectangle1.yMin = 210;
+    rectangle1.yMax = 239;
+    Graphics_setForegroundColor(&g_sContext, MMENU_BOTTOM_BAR_COLOR);
+    Graphics_fillRectangle (&g_sContext, &rectangle1);
+
+    // Draw background center circle
+    Graphics_setForegroundColor(&g_sContext, MMENU_TITLE_BACKGROUND_COLOR);
+    Graphics_fillCircle (&g_sContext, 160, 50, 14);
+    // Draw outer center circle
+    Graphics_setForegroundColor(&g_sContext, MMENU_BACKGROUND_COLOR);
+    Graphics_drawCircle (&g_sContext, 160, 50, 12);
+    // Draw inner center circle
+    Graphics_drawCircle (&g_sContext, 160, 50, 10);
+
+    //********** Draw information **************************************
+
+    // Switch which menu is being drawing
+    // Menu Title
+    Graphics_setForegroundColor(&g_sContext, MMENU_TITLE_COLOR);
+    Graphics_setFont(&g_sContext, &g_sFontCmss30b);
+    Graphics_drawStringCentered(&g_sContext, "System Setup",
+                                AUTO_STRING_LENGTH, 160, 19, GRAPHICS_TRANSPARENT_TEXT);
+
+    // Menu Information
+    Graphics_setForegroundColor(&g_sContext, MMENU_DATA_FONT_COLOR);
+    Graphics_setFont(&g_sContext, &g_sFontCmss20b);
+    Graphics_drawString(&g_sContext, "Circuit Mode",
+                                AUTO_STRING_LENGTH, 25, 64, GRAPHICS_TRANSPARENT_TEXT);
+    Graphics_drawString(&g_sContext, "Solenoid Mode",
+                                AUTO_STRING_LENGTH, 25, 88, GRAPHICS_TRANSPARENT_TEXT);
+    Graphics_drawString(&g_sContext, "Units Mode",
+                                AUTO_STRING_LENGTH, 25, 112, GRAPHICS_TRANSPARENT_TEXT);
+    Graphics_drawString(&g_sContext, "Controller Mode",
+                                AUTO_STRING_LENGTH, 25, 136, GRAPHICS_TRANSPARENT_TEXT);
+
+    // Menu active item
+    Graphics_setForegroundColor(&g_sContext, MMENU_TITLE_COLOR);
+    Graphics_fillCircle (&g_sContext, 17, 98, 4);
+
+    // Menu Data
+    Graphics_setForegroundColor(&g_sContext, MMENU_DATA_FONT_COLOR);
+    Graphics_drawString(&g_sContext, "CC",
+                                AUTO_STRING_LENGTH, 200, 64, GRAPHICS_TRANSPARENT_TEXT);
+    Graphics_drawString(&g_sContext, "Juergensen",
+                                AUTO_STRING_LENGTH, 200, 88, GRAPHICS_TRANSPARENT_TEXT);
+    Graphics_drawString(&g_sContext, "Imperial",
+                                AUTO_STRING_LENGTH, 200, 112, GRAPHICS_TRANSPARENT_TEXT);
+    Graphics_drawString(&g_sContext, "Primary",
+                                AUTO_STRING_LENGTH, 200, 136, GRAPHICS_TRANSPARENT_TEXT);
+    // Underline active data
+    rectangle1.xMin = 200;
+    rectangle1.xMax = 310;
+    rectangle1.yMin = 108;
+    rectangle1.yMax = 110;
+    Graphics_setForegroundColor(&g_sContext, MMENU_TITLE_COLOR);
+    Graphics_fillRectangle (&g_sContext, &rectangle1);
+
+    // Draw button function
+    Graphics_drawStringCentered(&g_sContext, "Next",
+                                AUTO_STRING_LENGTH, 35, 225, GRAPHICS_TRANSPARENT_TEXT);
+    Graphics_drawStringCentered(&g_sContext, "Select",
+                                AUTO_STRING_LENGTH, 285, 225, GRAPHICS_TRANSPARENT_TEXT);
+
 
 }
 
@@ -445,5 +585,5 @@ void timerInit()
 }
 
 void Delay(){
-    __delay_cycles(SYSTEM_CLOCK_SPEED * 2);
+    __delay_cycles(SYSTEM_CLOCK_SPEED );
 }
